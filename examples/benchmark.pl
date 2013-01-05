437dc4bbdf5bf7b09c13cfd36d82385bba058023
ugexe <nlogan@gmail.com>
2013-01-05 08:19:19 -0800

    comments that should crash, but timethis somehow 'runs'

--- examples/benchmark.pl
+++ examples/benchmark.pl
@@ -1,33 +1,69 @@
+use strict;
+use warnings;
 use Text::Levenshtein::Damerau::XS qw/xs_edistance cxs_edistance/;
 use Benchmark;
 use utf8;
 
-my @a = (1,2,3,4);
-my @b = (1,3,2,4,5);
+my $s2 = 1000;
+my $s3 = 100000;
+my $s4 = 10000000;
 
-my $x = "asdf";
-my $y = "adsfg";
+my @a1 = (1,2,3,4);
+my @b1 = (1,3,2,4,5);
+my @a2 = (1,2,3,4) x $s2;
+my @b2 = (1,3,2,4,5) x $s2;
+my @a3 = (1,2,3,4) x $s3;
+my @b3 = (1,3,2,4,5) x $s3;
+my @a4 = (1,2,3,4) x $s4;
+my @b4 = (1,3,2,4,5) x $s4;
+
+my $x1 = "asdf";
+my $y1 = "adsfg";
+my $x2 = "asdf" x $s2;
+my $y2 = "adsfg" x $s2;
+my $x3 = "asdf" x $s3;
+my $y3 = "adsfg" x $s3;
+my $x4 = "asdf" x $s4;
+my $y4 = "adsfg" x $s4;
+
+# Segmentation fault
+#print xs_edistance($x3,$y3) . "\n";
+
+# Out of Memory! (how? undef malloc used...)
+#print xs_edistance($x4,$y4) . "\n";
 
 print "\n------------------------------------------------\n";
+print "SMALL STRINGS (4*4 chars per string)\n";
 
-print "#xs small strings\n";
-timethis(1000000, 'xs_edistance($x,$y);'); 
-print "\n#cxs small strings\n";
-timethis(1000000, 'cxs_edistance(\@a,\@b,0);'); 
+print "#xs_edistance\n";
+timethis(1000000, 'xs_edistance($x1,$y1);'); 
+print "\n#cxs_edistance\n";
+timethis(1000000, 'cxs_edistance(\@a1,\@b1,0);'); 
+
+print "------------------------------------------------\n";
+print "MEDIUM STRINGS (4*1000 chars per string)\n";
+
+print "#xs_edistance\n";
+timethis(1000000, 'xs_edistance($x2,$y2);'); 
+print "\n#cxs_edistance\n";
+timethis(1000000, 'cxs_edistance(\@a2,\@b2,0);'); 
+
+print "------------------------------------------------\n";
+print "LARGE STRINGS (4*100,000 chars per string)\n";
+
+print "#xs_edistance\n";
+timethis(1000000, 'xs_edistance($x3,$y3);'); 
+print "\n#cxs_edistance\n";
+timethis(1000000, 'cxs_edistance(\@a3,\@b3,0);'); 
 
 print "------------------------------------------------\n";
 
-print "#xs medium strings\n";
-timethis(1000000, 'xs_edistance($x x 1000,$y x 1000);'); 
-print "\n#cxs medium strings\n";
-timethis(1000000, 'cxs_edistance(\@{(@a x 1000)},\@{(@b x 1000)},0);'); 
+print "HUGE STRINGS (4*10,000,000 chars pre string)\n";
 
-print "------------------------------------------------\n";
-
-print "#xs large strings\n";
-timethis(1000000, 'xs_edistance($x x 100000,$y x 100000);'); 
-print "\n#cxs medium strings\n";
-timethis(1000000, 'cxs_edistance(\@{(@a x 100000)},\@{(@b x 100000)},0);'); 
+print "#xs_edistance\n";
+timethis(1000000, 'xs_edistance($x4,$y4);'); 
+print "\n#cxs_edistance\n";
+timethis(1000000, 'cxs_edistance(\@a4,\@b4,0);'); 
 
 print "------------------------------------------------\n";
 
